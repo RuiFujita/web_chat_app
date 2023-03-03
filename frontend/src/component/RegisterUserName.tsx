a7c7ebe3f251d8f22ce4826eef1c25ce0b09a954
RuiFujita <Rui.Fujita@jp.ricoh.com>
2023-03-03 11:24:50 +0900

    テストコミット

--- frontend/src/component/RegisterUserName.tsx
+++ frontend/src/component/RegisterUserName.tsx
@@ -9,22 +9,48 @@ const RegisterUserName = () => {
   const [switchRoomList, setSwitchRoomList] = useState(false);
   const [userName, setUserName] = useState('');
 
-  const inputUserName = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setUserName(event.target.value);
-  }
+  const isDisabled = () => {
+    const AtoZ = '\\w'
+    const hiragana = '\\p{scx=Hiragana}'
+    const katakanaZen = '\\p{scx=Katakana}'
+    const katakanaHan = 'ｦ-ﾟ'
+    const kanji = '\\p{scx=Han}'
+    const mixRegex = (...regex: string[][]) => new RegExp('^[' + regex.join('') + ']+$', 'u')
 
-  const userNameCheck = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    event.preventDefault();
+    if (mixRegex([AtoZ, hiragana, katakanaZen, katakanaHan, kanji]).test(userName)) {
+      console.log(userName, ' は正しい入力です');
+    } else {
+      console.log(userName, ' は禁則文字です');
+    }
+
     const userNameMaxLength = 20;
     if (userName.length === 0) {
-      alert('名前を入力してください');
+      return true;
     } else if (userName.length > userNameMaxLength) {
-      alert('文字数制限を超えています(20字以下)');
-      setUserName('');
+      return true;
+    } else if (!mixRegex([AtoZ, hiragana, katakanaZen, katakanaHan, kanji]).test(userName)) {
+      return true;
     } else {
-      setSwitchRoomList(true);
+      return false;
     }
   }
+
+  // const inputUserName = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   setUserName(event.target.value);
+  // }
+
+  // const userNameCheck = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  //   event.preventDefault();
+  //   const userNameMaxLength = 20;
+  //   if (userName.length === 0) {
+  //     alert('名前を入力してください');
+  //   } else if (userName.length > userNameMaxLength) {
+  //     alert('文字数制限を超えています(20字以下)');
+  //     setUserName('');
+  //   } else {
+  //     setSwitchRoomList(true);
+  //   }
+  // }
 
   return (
     <div>
@@ -32,13 +58,13 @@ const RegisterUserName = () => {
         <div>
           <div className='welcome-text'>ようこそ</div>
           <div className='input-user-name'>
-              <input
-                type='text'
-                value={userName}
-                onChange={inputUserName}
-                placeholder='名前を入力してください'
-              />
-              <button className='register-button' onClick={userNameCheck}>登録</button>
+            <input
+              type='text'
+              value={userName}
+              onChange={(event) => setUserName(event.target.value)}
+              placeholder='名前を入力してください'
+            />
+            <button className='register-button' onClick={() => setSwitchRoomList(true)} disabled={isDisabled()}>登録</button>
           </div>
         </div>
       }
