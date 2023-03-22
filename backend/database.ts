0a283528054235feca4587afcccb6513f5dfabf6
Rui.Fujita <Rui.Fujita@jp.ricoh.com>
2023-03-22 13:03:22 +0900

    該当箇所を削除

--- backend/database.ts
+++ backend/database.ts
@@ -34,7 +34,7 @@ app.post('/new_room', (request, response) => {
   connection.connect();
 
   const roomName = request.body.roomName;
-  connection.query(`INSERT INTO room_info (room_name, is_deleted) VALUES ('${roomName}', 0)`, function (error: any, results: []) {
+  connection.query(`INSERT INTO room_info (room_name) VALUES ('${roomName}')`, function (error: any, results: []) {
     if (error) throw error;
     response.status(200).send(results);
   });
